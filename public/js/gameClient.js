ac74de0f1cecb768d75f6d8a3f3aaf3f890a9c30
Italo <italo_07_@hotmail.com>
2016-02-10 09:54:46 -0300

    modificação no canvas e nos circulos

--- public/js/gameClient.js
+++ public/js/gameClient.js
@@ -22,3 +22,18 @@ function Line(point1,point2){
 function Square(){
     
 };
+
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+
+var x = 250;
+var y = 70;
+
+        
+    for(x=250;x<=850;x+=100){ 
+        for(y=70;y<=560;y+=70){
+            ctx.beginPath();
+            ctx.arc(x,y,5,0,2*Math.PI);
+            ctx.fill();
+        } 
+    }
