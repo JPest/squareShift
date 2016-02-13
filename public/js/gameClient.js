1dee89f9c74f8d52ca3142e2e7c030d12a5318fe
Jagni Dasa <jagnidasa@gmail.com>
2016-02-13 18:23:44 -0300

    Animação de entrada dos pontos e checagem de linhas repetidas

--- public/js/gameClient.js
+++ public/js/gameClient.js
@@ -1,44 +1,57 @@
 //Objetos do Game
 var game = {
-    squares:[],//Quadrados aqui
-    filledSquareCount:0,
-    roundPlayer:[],//Jogadores aqui
-    validateMove: function (point1, point2){
-                        var a = Math.abs(point1.crdX - point2.crdX);
-                        var b = Math.abs(point1.crdY - point2.crdY);
-                        if (a>0 && b>0){
-                            return false;
-                        }else if(a>100 || b>100){
-                            return false;
-                        }else{
-                            return true;
-                        }
-                    }    
-        
+    squares: [], //Quadrados aqui
+    lines: [],
+    points: [],
+    filledSquareCount: 0,
+    roundPlayer: [], //Jogadores aqui
+    validateMove: function (point1, point2) {
+        var a = Math.abs(point1.crdX - point2.crdX);
+        var b = Math.abs(point1.crdY - point2.crdY);
+
+        for (line of game.lines) {
+            if (line.ponint1.crdX == point1.crdX && line.ponint1.crdY == point1.crdY &&
+                line.ponint2.crdX == point2.crdX && line.ponint2.crdY == point2.crdY) {
+
+                return false;
+
+            }
+        }
+
+
+        if (a > 0 && b > 0) {
+            return false;
+        } else if (a > 100 || b > 100) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
 };
 
-function Player(name,color){
+function Player(name, color) {
     this.name = name;
     this.color = color;
     this.score = 0;
 };
 
 
-function Point(crdX,crdY){
+function Point(crdX, crdY) {
     this.crdX = crdX;
     this.crdY = crdY;
 };
 
-function Line(point1,point2){
+function Line(point1, point2) {
     this.ponint1 = point1;
     this.ponint2 = point2;
 };
 
-function Square(){
+function Square() {
     this.filled = false;
     this.color = "";
-    this.points =[];// Pontos aqui
-    this.lines = [];// Linhas aqui
+    this.points = []; // Pontos aqui
+    this.lines = []; // Linhas aqui
 };
 
 //Canvas
@@ -53,52 +66,61 @@ stage.enableMouseOver(20);
 
 var connection = null;
 
-var i=0;
-var j=0;
+var i = 0;
+var j = 0;
 
-var myPoints = [] ;
+var myPoints = [];
 
 //j=70;j<=560;j+=100
 //i=250;i<=850;i+=100
 
-
-for(j=70;j<=560;j+=100){
+var pointTime = 50;
+for (j = 70; j <= 560; j += 100) {
     var auxX = 0;
-    for(i=250;i<=850;i+=100){
+    for (i = 250; i <= 850; i += 100) {
         var _i = i;
         var _j = j;
-        
-        (function() {
+
+        (function () {
             var circle = new createjs.Shape().set({
                 x: _i,
                 y: _j,
                 cursor: "pointer",
-                name:"target"
+                name: "target"
 
             });
             circle.graphics.f(createjs.Graphics.getRGB("fff"))
-                .dc(0,0,6);
+                .dc(0, 0, 6);
             stage.addChild(circle);
-            var new_point = new Point(_i,_j);
-            circle.on("mousedown", function(e) {
+
+            circle.scaleX = 0;
+            circle.scaleY = 0;
+            createjs.Tween.get(circle).wait(pointTime).to({
+                scaleX: 1,
+                scaleY: 1,
+            }, 1000, createjs.Ease.elasticOut);
+            pointTime += 50;
+
+            var new_point = new Point(_i, _j);
+            circle.on("mousedown", function (e) {
                 mousePress(e, new_point);
             });
             myPoints.push(new_point);
         })();
-    } 
+    }
 }
 
 function mousePress(event, point) {
     connection = new createjs.Shape().set({
-        x:event.target.x, 
-        y:event.target.y,
-        mouseEnabled:false,
-        graphics: new createjs.Graphics().s("#00f").dc(0,0,50)
+        x: event.target.x,
+        y: event.target.y,
+        mouseEnabled: false,
+        graphics: new createjs.Graphics().s("#00f").dc(0, 0, 50)
     });
     stage.addChild(connection);
     stage.addEventListener("stagemousemove", desenhaLinha);
     stage.addEventListener("stagemouseup", mouse_point_getter);
-                           
+
     function mouse_point_getter(e) {
         fimLinha(e, point);
         stage.removeEventListener("stagemouseup", mouse_point_getter);
@@ -108,38 +130,44 @@ function mousePress(event, point) {
 function desenhaLinha(event) {
     connection.graphics.clear()
         .s("#f00")
-        .mt(0,0).lt(stage.mouseX-connection.x, stage.mouseY-connection.y);
+        .mt(0, 0).lt(stage.mouseX - connection.x, stage.mouseY - connection.y);
 }
 
 function fimLinha(event, first_point) {
     var target, targets = stage.getObjectsUnderPoint(stage.mouseX, stage.mouseY);
-    for (var i=0; i<targets.length; i++) {
-        if (targets[i].name == "target") { 
+    for (var i = 0; i < targets.length; i++) {
+        if (targets[i].name == "target") {
             target = targets[i];
-            console.log("te amo"); 
-            break; 
-        }   
+            console.log("te amo");
+            break;
+        }
     }
-    
+
     var last_point;
-    if(target) {
-        myPoints.filter(function(el) {
-            if(el.crdX == target.x && el.crdY == target.y)
+    if (target) {
+        myPoints.filter(function (el) {
+            if (el.crdX == target.x && el.crdY == target.y)
                 last_point = el;
         });
     }
-    
+
     if (target != null && game.validateMove(first_point, last_point)) {
         connection.graphics.clear()
-        .s("red")
-        .mt(0,0).lt(target.x-connection.x, target.y-connection.y);
-        
+            .s("red")
+            .mt(0, 0).lt(target.x - connection.x, target.y - connection.y);
+        var line = {
+            ponint1: first_point,
+            ponint2: last_point
+        };
+
+        game.lines.push(line);
+
     } else {
         stage.removeChild(connection);
     }
-    
+
     stage.removeEventListener("stagemousemove", desenhaLinha);
-    
+
 }
 
 function tick(event) {
